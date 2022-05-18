ebb4f3cbc5fcca7490dac3a6c98eef5b8072364b
Jurijus Luganskis <j_luganskis@yahoo.com>
2022-05-18 21:00:33 +0300

    dogOwners

--- JS Tasks/Task 5/script.js
+++ JS Tasks/Task 5/script.js
@@ -19,3 +19,8 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+function filterDogOwers() {
+  const dogOwners = users.filter(users => users.hasDog);
+  console.log(dogOwners);
+}
