109ba14d78bcfa378883ae4bb5637b1bc72626d1
Jurijus Luganskis <j_luganskis@yahoo.com>
2022-05-21 09:59:33 +0300

    Task No.9 in progress

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -10,3 +10,32 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const fetchCarsList = async (brand, models) => {
+    const response = await fetch(ENDPOINT);
+    const carsList = await response.json();
+
+console.log(carsList);
+carsConttainer(carsList);
+
+}
+
+function carsConttainer(carsList) {
+    const addCarsList = document.getElementById('output');
+    addCarsList.innerText = '';
+
+    carsList.forEach((car) => {
+
+        const carBrand = document.createElement('div');
+        carBrand.innerText = car.brand;
+        carBrand.style.fontSize = '5rem';
+        carBrand.style.color = 'red';
+        carBrand.style.margin = '5rem';         
+       
+        const carModels = document.createElement('div');
+        carModels.innerText = car.models;
+        carModels.style.margin = '2 2 rem';
+        
+        addCarsList.append(carBrand, carModels);
+    });
+}
